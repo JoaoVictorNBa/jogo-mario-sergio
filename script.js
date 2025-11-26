15e731c6e3f503476a440e73dadb8a71ebab1833
JoaoVictorNBa <bagatimjoao@gmail.com>
2025-11-26 12:59:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const mario = document.querySelector('.mario');
+const fogo = document.querySelector('.fogo');
+
+const saltar = () => {
+        mario.classList.add('saltar');
+        
+        setTimeout(() => {
+            mario.classList.remove('saltar');
+        }, 500);
+}
+
+
+// Novo nome do loop de monitoramento: lacoloop
+const lacoloop = setInterval(()=>{
+    const posicaofogo = fogo.offsetLeft; 
+    const alturamario = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (posicaofogo <= 100 && posicaofogo > 0 && alturamario < 80) { 
+
+        fogo.style.animation = 'none';
+        fogo.style.left = `${posicaofogo}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${alturamario}px`;
+        
+        mario.src = 'img/game-over.png';
+        mario.style.width ='90px';
+        mario.style.marginLeft = '30px';
+        
+        clearInterval(lacoloop);
+    }
+}, 15);
+
+document.addEventListener('keydown', evento => {
+    if (evento.code === 'Space') {
+        saltar();
+    }
+});
