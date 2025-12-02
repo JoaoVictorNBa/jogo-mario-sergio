cc3096d739341507d0ccba3ce66ace45de5836da
JoaoVictorNBa <bagatimjoao@gmail.com>
2025-12-01 23:15:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-const mario = document.querySelector('.mario');
+const pkx = document.querySelector('.pkx');
 const fogo = document.querySelector('.fogo');
 
 const saltar = () => {
-        mario.classList.add('saltar');
+        pkx.classList.add('saltar');
         
         setTimeout(() => {
-            mario.classList.remove('saltar');
+            pkx.classList.remove('saltar');
         }, 500);
 }
 
 const lacoloop = setInterval(()=>{
     const posicaofogo = fogo.offsetLeft; 
-    const alturamario = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const alturapkx = +window.getComputedStyle(pkx).bottom.replace('px', '');
 
-    if (posicaofogo <= 100 && posicaofogo > 0 && alturamario < 80) { 
+    if (posicaofogo <= 100 && posicaofogo > 0 && alturapkx < 80) { 
 
         fogo.style.animation = 'none';
         fogo.style.left = `${posicaofogo}px`;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${alturamario}px`;
+        pkx.style.animation = 'none';
+        pkx.style.bottom = `${alturapkx}px`;
         
-        mario.src = 'img/game-over.png';
-        mario.style.width ='90px';
-        mario.style.marginLeft = '30px';
+        pkx.src = 'img/game-over.png';
+        pkx.style.width ='90px';
+        pkx.style.marginLeft = '30px';
         
         clearInterval(lacoloop);
     }
@@ -35,4 +35,5 @@ document.addEventListener('keydown', evento => {
     }
 
 });
+
 
