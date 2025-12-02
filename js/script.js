7dd356b4df061bbb0673c20812098219b5246f0f
JoaoVictorNBa <bagatimjoao@gmail.com>
2025-12-01 23:22:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ const lacoloop = setInterval(()=>{
         pkx.style.animation = 'none';
         pkx.style.bottom = `${alturapkx}px`;
         
-        pkx.src = 'img/imagens.jpeg';
+        pkx.src = 'img/images.jpeg';
         pkx.style.width ='90px';
         pkx.style.marginLeft = '30px';
         
@@ -35,6 +35,7 @@ document.addEventListener('keydown', evento => {
     }
 
 });
+
 
 
 
